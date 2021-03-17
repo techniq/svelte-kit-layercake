6d66bbc255a47f0d3e05cf9c4ab76d1708adfca3
Sean Lynch <techniq35@gmail.com>
2021-03-17 16:02:16 -0400

    Initial commit

---
+++ src/lib/Chart.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { LayerCake, Svg } from "layercake";
+
+  import Line from "./Line.svelte";
+  import Area from "./Area.svelte";
+  import AxisX from "./AxisX.svelte";
+  import AxisY from "./AxisY.svelte";
+
+  import data from "./points";
+
+  const xKey = "myX";
+  const yKey = "myY";
+
+  data.forEach((d) => {
+    d[yKey] = +d[yKey];
+  });
+</script>
+
+<div class="chart-container">
+  <LayerCake
+    padding={{ right: 10, bottom: 20, left: 25 }}
+    x={xKey}
+    y={yKey}
+    yDomain={[0, null]}
+    {data}
+  >
+    <Svg>
+      <AxisX />
+      <AxisY ticks={4} />
+      <Line />
+      <Area />
+    </Svg>
+  </LayerCake>
+</div>
+
+<style>
+  /*
+    The wrapper div needs to have an explicit width and height in CSS.
+    It can also be a flexbox child or CSS grid element.
+    The point being it needs dimensions since the <LayerCake> element will
+    expand to fill it.
+  */
+  .chart-container {
+    width: 500px;
+    height: 500px;
+  }
+</style>
